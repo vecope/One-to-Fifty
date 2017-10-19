82c2be459dab2ff6fe8fc536de38bba343556475
Camilo Alfonso <6633004+vecope@users.noreply.github.com>
2017-10-19 14:17:15 -0500

    Basic usage of UITableView
    This is a basic project developed during the course of Udemy's iOS 11 complete course by Rob Percival

--- One to Fifty/ViewController.swift
+++ One to Fifty/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default , reuseIdentifier: "Cell")
+        cell.textLabel?.text = String(indexPath.row+1)
+        return cell
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
